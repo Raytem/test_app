c76a8428077fbeacf28e82bfadc7e3a585ad8e36
Daniil <Danik1320@yandex.by>
2023-12-23 12:34:51 +0300

    .

--- src/realizations/user/user.service.mjs
+++ src/realizations/user/user.service.mjs
@@ -6,84 +6,60 @@ import { UserModel } from "./model/user.model.mjs";
 class UserService {
 
   async increaseBalance(userId, changeBalanceDto) {
-    let updateData = null;
+    let updatedUser = null;
 
-    const retryFunction = async () => await sequelize.transaction({
-      isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE,
-    }, async (t) => {
-      const user = await UserModel.findByPk(userId, {
-        plain: true, 
-        transaction: t,
-      });
-      if (!user) {
-        throw ApiError.NotFound('no such user');
-      }
-
-      const newUserBalance = user.balance + changeBalanceDto.amount;
+    const user = await UserModel.findByPk(userId, {
+      plain: true, 
+    });
+    if (!user) {
+      throw ApiError.NotFound('no such user');
+    }
       
-      updateData = await UserModel.update(
-        { balance: newUserBalance },
+    await sequelize.transaction({
+    }, async (t) => {
+      const updateData = await UserModel.update(
+        { balance: sequelize.literal(`"balance" + ${changeBalanceDto.amount}`) },
         { where: { id: userId }, 
           returning: true, 
           plain: true, 
           transaction: t,
         },
       );
-    })
+      
+      updatedUser = updateData[1].dataValues;
+    })      
 
-    try {
-      await retryFunction();
-     } catch (e) {
-       if (e instanceof ApiError === false) {
-         await retryFunction();
-       } else {
-        throw e;
-      }
-     }
-
-    return updateData[1].dataValues;
+    return updatedUser;
   }
 
   async decreaseBalance(userId, changeBalanceDto) {
-    let updateData = null;
+    let updatedUser = null;
 
-    const retryFunction = async () => await sequelize.transaction({
-      isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE,
+    const user = await UserModel.findByPk(userId, {
+      plain: true,
+    });
+    if (!user) {
+      throw ApiError.NotFound('no such user');
+    }
+
+    await sequelize.transaction({
     }, async (t) => {
-      const user = await UserModel.findByPk(userId, {
-        plain: true,
-        transaction: t,
-      });
-      if (!user) {
-        throw ApiError.NotFound('no such user');
-      }
-
-      const newUserBalance = user.balance - changeBalanceDto.amount;
-      if (newUserBalance < 0) {
-        throw ApiError.BadRequestError('there are not enough funds');
-      }
-      
-      updateData = await UserModel.update(
-        { balance: newUserBalance },
+      const updateData = await UserModel.update(
+        { balance: sequelize.literal(`"balance" - ${changeBalanceDto.amount}`) },
         { where: { id: userId }, 
           returning: true, 
           plain: true, 
           transaction: t,
         },
       );
-    })
-
-    try {
-     await retryFunction();
-    } catch (e) {
-      if (e instanceof ApiError === false) {
-        await retryFunction();
-      } else {
-        throw e;
+      
+      updatedUser = updateData[1].dataValues;
+      if (updatedUser.balance < 0) {
+        throw ApiError.BadRequestError('there are not enough funds');
       }
-    }
+    })      
 
-    return updateData[1].dataValues;
+    return updatedUser;
   }
 }
 
